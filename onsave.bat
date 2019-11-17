4612df2e20af104b8fde0c315817e4c21f3bef45
shadow578 <christophnienhaus2@gmail.com>
2019-11-17 13:53:05 +0100

    Update SaveScript

--- onsave.bat
+++ onsave.bat
@@ -1,4 +1,21 @@
+@echo off
 REM Example PadoruManager SaveScript.
 REM use %1 to get the directory the collection is in
 REM like this
 echo Collection dir: %1
+
+REM cd into collection dir (=git repo dir)
+cd %1
+
+REM add all (new) files to git
+git add -A
+
+REM create git commit, ask user for commit message
+git commit
+
+REM push commit to remote
+REM git push -u origin master
+git push
+
+REM debug pause
+pause
